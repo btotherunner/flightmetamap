4bef7829f69d5f4966545a609af61c0f7a9f2d07
Markus Brunner <info@brunner-it.de>
2023-01-11 11:34:12 +0100

    Updates

--- meta_soup.py
+++ meta_soup.py
@@ -7,18 +7,17 @@ from datetime import datetime
 from paho.mqtt import client as mqtt_client
 
 ICAO = ['EDAC',	'EDAH',	'EDBC',	'EDDB',	'EDDC',	'EDDE',	'EDDF',	'EDDG',	'EDDH',	'EDDK',	'EDDL',	'EDDM',	'EDDN',	'EDDP',	'EDDR',	'EDDS',	'EDDV',	'EDDW',	'EDFH',	'EDFM',	'EDGS',	'EDHI',	'EDHK',	'EDHL',	'EDJA',	'EDLN',	'EDLP',	'EDLV',	'EDLW',	'EDMA',	'EDMO',	'EDNY',	'EDQM',	'EDSB',	'EDTL',	'EDTY',	'EDVE',	'EDVK',	'EDXW',	'ETAD',	'ETAR',	'ETEB',	'ETGG',	'ETHA',	'ETHB',	'ETHC',	'ETHF',	'ETHL',	'ETHN',	'ETHS',	'ETIC',	'ETIH',	'ETIK',	'ETMN',	'ETND',	'ETNG',	'ETNH',	'ETNL',	'ETNN',	'ETNS',	'ETNT',	'ETNW',	'ETOU',	'ETSB',	'ETSH',	'ETSI',	'ETSL',	'ETSN',	'ETWM']
-#ICAO = ['EDAC',	'EDAH',	'EDBC',	'EDDB']
+ICAO = ['EDDR','ETAR','EDFH','EDDF','EDFM','EDSB','EDDS','EDTY','ETHN']
 
 VFR_STATUS = "NONE"
 
 # current date and time
 
 
-
-
 def getVfrStatus(ICAO):
     now = datetime.utcnow()
     html_text = requests.get('https://www.aviationweather.gov/metar/data?ids='+ICAO+'&format=decoded&hours=0&taf=off&layout=off').text
+    time.sleep(0.5)
     soup = BeautifulSoup(html_text, 'lxml')
 
     raw_data = soup.find_all('td')
@@ -30,13 +29,13 @@ def getVfrStatus(ICAO):
 
 
     t1 = datetime(year=2023, month=1, day=int(meta_date), hour=int(meta_hour), minute=int(meta_minute))
-    #print(t1)
+    print(t1)
     t2 = datetime(year=2023, month=1, day=int(now.strftime("%d")), hour=int(now.strftime("%H")), minute=int(now.strftime("%M")))
-    #print(t2)
+    print(t2)
 
     time_diff = t2 - t1
     #print("Zeitdifferenz:")
-    #print(time_diff.total_seconds())
+    print(time_diff.total_seconds())
  
     s1 = now.strftime("%m/%d/%Y, %H:%M:%S")
     # mm/dd/YY H:M:S format
@@ -44,7 +43,7 @@ def getVfrStatus(ICAO):
  
 
     #print(len(raw_data))
-    if(time_diff.total_seconds() < 2400):
+    if(time_diff.total_seconds() < 86400):
         if(len(raw_data) >= 18):
             if(raw_data[15].text == "ceiling and visibility are OK"):
                 CEIL = int(10000)
@@ -71,15 +70,15 @@ def getVfrStatus(ICAO):
         VFR_STATUS = "VFR"
     elif (VIS >= 3 and (CEIL > 1000)):
         VFR_STATUS = "MVFR"
-    elif (VIS >= 1 and (CEIL > 500)):
+    elif (VIS >= 1 and (CEIL >= 500)):
         VFR_STATUS = "IFR"
-    elif (VIS < 1 or (CEIL < 500)):
+    elif (VIS < 1 or (CEIL <= 500)):
         VFR_STATUS = "LIFR"
     else:
         VFR_STATUS = "NONE"
 
-    print(ICAO, VFR_STATUS)    
-    #print(ICAO, VFR_STATUS, VIS, CEIL)
+    #print(ICAO, VFR_STATUS)    
+    print(ICAO, VFR_STATUS, VIS, CEIL)
     
     return VFR_STATUS
 
