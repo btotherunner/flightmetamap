af50473386e0dc65cbe114df540422eaa3ac6c22
Markus Brunner <info@brunner-it.de>
2023-01-14 07:06:32 +0100

    Angepasst auf kleine Karte

--- meta_soup.py
+++ meta_soup.py
@@ -6,39 +6,38 @@ from datetime import datetime
 
 from paho.mqtt import client as mqtt_client
 
-#ICAO = ['EDAC','EDAH',	'EDBC',	'EDDB',	'EDDC',	'EDDE',	'EDDF',	'EDDG',	'EDDH',	'EDDK',	'EDDL',	'EDDM',	'EDDN',	'EDDP',	'EDDR',	'EDDS',	'EDDV',	'EDDW',	'EDFH',	'EDFM',	'EDGS',	'EDHI',	'EDHK',	'EDHL',	'EDJA',	'EDLN',	'EDLP',	'EDLV',	'EDLW',	'EDMA',	'EDMO',	'EDNY',	'EDQM',	'EDSB',	'EDTL',	'EDTY',	'EDVE',	'EDVK',	'EDXW',	'ETAD',	'ETAR',	'ETEB',	'ETGG',	'ETHA',	'ETHB',	'ETHC',	'ETHF',	'ETHL',	'ETHN',	'ETHS',	'ETIC',	'ETIH',	'ETIK',	'ETMN',	'ETND',	'ETNG',	'ETNH',	'ETNL',	'ETNN',	'ETNS',	'ETNT',	'ETNW',	'ETOU',	'ETSB',	'ETSH',	'ETSI',	'ETSL',	'ETSN',	'ETWM']
+ICAO = ['EDAC',	'EDAH',	'EDBC',	'EDDB',	'EDDC',	'EDDE',	'EDDF',	'EDDG',	'EDDH',	'EDDK',	'EDDL',	'EDDM',	'EDDN',	'EDDP',	'EDDR',	'EDDS',	'EDDV',	'EDDW',	'EDFH',	'EDFM',	'EDGS',	'EDHI',	'EDHK',	'EDHL',	'EDJA',	'EDLN',	'EDLP',	'EDLV',	'EDLW',	'EDMA',	'EDMO',	'EDNY',	'EDQM',	'EDSB',	'EDTL',	'EDTY',	'EDVE',	'EDVK',	'EDXW',	'ETAD',	'ETAR',	'ETEB',	'ETGG',	'ETHA',	'ETHB',	'ETHC',	'ETHF',	'ETHL',	'ETHN',	'ETHS',	'ETIC',	'ETIH',	'ETIK',	'ETMN',	'ETND',	'ETNG',	'ETNH',	'ETNL',	'ETNN',	'ETNS',	'ETNT',	'ETNW',	'ETOU',	'ETSB',	'ETSH',	'ETSI',	'ETSL',	'ETSN',	'ETWM']
 ICAO = ['EDDR','ETAR','EDFH','EDDF','EDFM','EDSB','EDDS','EDTY','ETHN']
+ICAO = ['EDDL', 'EDDN', 'EDDK', 'ETAD', 'ETSB', 'EDFH', 'ETAR', 'EDDR', 'EDTL', 'EDTL', 'EDSB', 'EDFM', 'EDDF', 'ETHF', 'EDVK', 'EDLP', 'EDBC', 'EDDE', 'EDDP', 'ETSH', 'EDDC', 'EDAC', 'EDQM', 'LKKV', 'ETIC',  'ETHN', 'ETIK', 'EDDN', 'ETSI', 'EDMA', 'EDDM', 'ETHA', 'EDNY', 'EDDS', 'ETHL', 'EDJA']
 
 VFR_STATUS = "NONE"
 
-broker = '10.10.11.151'
-port = 1883
-# username = 'emqx'
-# password = 'public'
+# current date and time
 
 
 def getVfrStatus(ICAO):
     now = datetime.utcnow()
     html_text = requests.get('https://www.aviationweather.gov/metar/data?ids='+ICAO+'&format=decoded&hours=0&taf=off&layout=off').text
-    time.sleep(0.5)
+    time.sleep(0.2)
     soup = BeautifulSoup(html_text, 'lxml')
 
     raw_data = soup.find_all('td')
 
     split_time = raw_data[3].text.split()
+    #print(split_time)
     meta_date = split_time[1][0:2]
     meta_hour = split_time[1][2:4]
     meta_minute = split_time[1][4:6]
 
 
     t1 = datetime(year=2023, month=1, day=int(meta_date), hour=int(meta_hour), minute=int(meta_minute))
-    # print(t1)
+    #print(t1)
     t2 = datetime(year=2023, month=1, day=int(now.strftime("%d")), hour=int(now.strftime("%H")), minute=int(now.strftime("%M")))
-    # print(t2)
+    #print(t2)
 
     time_diff = t2 - t1
     #print("Zeitdifferenz:")
-    # print(time_diff.total_seconds())
+    #print(time_diff.total_seconds())
  
     s1 = now.strftime("%m/%d/%Y, %H:%M:%S")
     # mm/dd/YY H:M:S format
@@ -57,11 +56,11 @@ def getVfrStatus(ICAO):
                 CEIL = int(split_ceil[0])
         else:
             VFR_STATUS = "NONE"
-            #print(ICAO, VFR_STATUS) 
+            print(ICAO, VFR_STATUS) 
             return VFR_STATUS
     else:
         VFR_STATUS = "NONE"
-        #print(ICAO, VFR_STATUS) 
+        print(ICAO, VFR_STATUS) 
         return VFR_STATUS
     
     split_vis = raw_data[13].text.split()
@@ -85,9 +84,13 @@ def getVfrStatus(ICAO):
     
     return VFR_STATUS
 
+broker = '10.10.11.151'
+port = 1883
+
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
-
+# username = 'emqx'
+# password = 'public'
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -118,6 +121,7 @@ def publish(client, airports_count, status):
         #time.sleep(1)
         topic = "cmnd/weathermap/led"+str(airports_count)
         result = client.publish(topic, msg)
+	#send_timestamp = client.publish("cmnd/weathermap/display", s1)
         # result: [0, 1]
         status = result[0]
         if status == 0:
@@ -126,6 +130,7 @@ def publish(client, airports_count, status):
         else:
             print(f"Failed to send message to topic {topic}")
             return
+
 
 
 def run():
@@ -145,3 +150,4 @@ if __name__ == '__main__':
 #sudo pip3 install rpi_ws281x
 #sudo pip3 install adafruit-circuitpython-neopixel
 #sudo python3 -m pip install --force-reinstall adafruit-blinka
+ 
